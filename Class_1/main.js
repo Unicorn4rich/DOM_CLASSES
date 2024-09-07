ffd3100b60f23af58a55dd8dc193910d0927b9ef
Dream_4r <145902745+Unicorn4rich@users.noreply.github.com>
2024-09-08 00:15:35 +0500

    class 1

---
+++ Class_1/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Elelement Selector
+let myDiv = document.getElementById('myDiv');
+/* myDiv.innerText = "I am Div" */ /* Div ke andar text likha hai is property se */
+console.log(myDiv);
---
+++ Class_1/main.ts
@@ -0,0 +1,8 @@
+// Elelement Selector
+
+let myDiv = document.getElementById('myDiv');
+
+/* myDiv.innerText = "I am Div" */          /* Div ke andar text likha hai is property se */
+
+console.log(myDiv);
+
