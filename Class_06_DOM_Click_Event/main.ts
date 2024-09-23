98192541eb6317c1cb7396b34a956f5028e3cbef
Dream_4r <145902745+Unicorn4rich@users.noreply.github.com>
2024-09-24 00:02:44 +0500

    new adding

---
+++ Class_06_DOM_Click_Event/main.ts
@@ -0,0 +1,106 @@
+let myButton1 = document.getElementById('myButton1');
+let myButton2 = document.getElementById('myButton2');
+
+// data type dey kar Element ko get kiyya.
+let myPara: HTMLElement | null = document.getElementById('myPara');
+
+console.log(myPara);  // null
+
+// example 1
+
+myButton1?.addEventListener('click', ()=>{
+    console.log("Button kaam kar rha hai Saamha....") // button1 click hony par browser console mein iska output show hoga.
+});
+
+
+// example 2
+
+myButton1?.addEventListener('click', ()=>{
+    alert("Bohut khatar naak log hain ye to😢") // click par alert message show hoga ye browser pe
+});
+
+
+// example 3
+
+myButton1?.addEventListener('click', ()=>{
+    document.body.style.background = "yellow"; // button1 click hony par body ka backgroud color yellow kar dega.
+});
+
+myButton2?.addEventListener('click', ()=>{
+    document.body.style.background = "red";    // button2 click hony par body ka backgroud color red kar dega.
+})
+
+
+// example 3
+
+myButton1?.addEventListener('click', ()=>{
+    myButton2!.style.display = "none";        // button1 par click karne se button2 ghaib ho jaega. or ye button2 mein (!) ka sign error htany ke liye lgaya hai.
+});
+
+myButton2?.addEventListener('click', ()=>{
+    myButton1!.style.display = "none";       // button2 par click karne se button1 ghaib ho jaega.
+});
+
+
+// example 4
+
+myButton1?.addEventListener('click', ()=>{      // button1 pe click karne se Sir ne in dono ko apas mein larwa diyya
+    myButton2!.style.background = "red";        // button2 ka background color red ho jaega or
+    myButton1!.style.background = "white";      // button1 ka background color white ho jaega jo ke ye khud hai.  khud ka background color 
+});                                             // isliye white kar rha hai kiyun ke jab dosra button click hua hoga to iska color change kar 
+                                                // diyye hoga isliye ye button jese hi click hoga dosry button ke color ke sath sath khud ka color bhi change kar lega.
+
+
+myButton2?.addEventListener('click', ()=>{      // button2 click
+    myButton1!.style.background = "green";      // button1 ka background color green ho jaega or
+    myButton2!.style.background = "white";      // button2 jo ye khud hai apna color ye white kar lega.
+})
+
+
+// example 5
+
+myButton1?.addEventListener('click', ()=>{        // Button1 pe jese hi click hoga 
+    myButton1!.textContent = "I am clicked...."   // Khud ka jo Text hai usy change kar ke apne andar (I am clicked..) likh dega.
+})
+
+
+// myPara work manipulation
+
+myPara.addEventListener('click', ()=>{
+    myPara.textContent = "I am clicked";
+})
+
+
+
+
+
+/* NOTES 
+
+
+myButton?.
+
+button mein (?) isliye likha hai kiyun ke hamen nahi pata jo iski value hai wo null bhi ho sakti hai  isliye ham ? se
+ye optional bana dety hain ke iski value kuch bhi ho jaye jaye or age iski jagah (!) not ka sign laga den to ham 
+typesript ko ye kehty hain ke hamen pata hai ke iski value null nahi ho sakti. 
+or agr ham in dono mein se koi bhi nahi lgana chahty to opar document.getElement wali line mein jaa kar veriable ko any data type dey denge.
+
+addEventListener('click', ()=>{})
+
+ye method aik Event hai jo hamen ijazat deta hai event lagane ki iske andar bohut sary events hoty hain jinko use kar ke 
+ham alag alag event bana kar perform kar sakty hain or ye event apne andar single ya double qutations leta hai. 
+ye Event apne andar 2 values leta hai 1st value wo event hota hai jo ham bnana chahty hain or dosri value aik function 
+bana kar lgate hain jese hi ye dono values (addEventListener) ko dety hain to ye hmara aik click Event ban jata hai
+iske arrow function ka andar ham jo bhi code likhenge wo tab chalega jab is event button pr click parega.
+
+
+let myPara: HTMLElement | null = document.getElementById('myPara');
+
+jab bhi ham (HTML) se koi bhi Element (Typescript) mein get karte hain to typescript hamen aik error show krwata hai jisme 
+wo ye keh rha hota hai ('Element_Name' is possibly 'null') iska matlab ye hai ke typescript ka compiler phupo ban jata hai
+usy ye tension stati rehti hai ke hamne jo Element HTML se get kiyya hai wo agr HTML mein ho hi naaa to phirrr....? isliye
+is situation mein typescript hmare Element ko null print karega is cheez ko handle karne ke liye mein ham  us Element ko (?)
+laga kar optinal bana dety hain ke agr hmara (Element) apko nahi milta to ap is Event ko ignore kar dena chlana hi mat.
+ya phir (!) not ka sign laga kar typescript ko ye keh sakty hain ke jesa tum soch rhy ho wesa bilkul nahi hai
+ya phirrr ham direct us Element ke veriable ko hi any data type dey dety hain ke ye element kuch bhi hamen faraq nahi parta.
+
+*/
---
+++ Class_07_DOM_E_Target/main.ts
@@ -0,0 +1,34 @@
+let btn = document.getElementById('btn');
+let btn2 = document.getElementById('btn2');
+let myInput = document.getElementById('myInput');
+
+
+// btn?.addEventListener('click', (e)=>{   // function ke parameter (e) mein hamne addEvent ki sari property`s print krwa ke dekh li ke uske andar jitni bhi property`s hain..
+//    console.log(e.target);  // yahn par ham (e) ke event mein se uski target ki value ko get kar ke print krwa rhy hain ke hamen bas (target) ki value print hui huwi chahiye.
+// });                        // ye (target) ki property khud ko dhond kar laa kar deti hai. matlab ke jis HTML element ko ham get kar ke uspe ye property lgaenge to ye  
+                           // us HTML Element ko print krwa ke dikha degi ke wo HTML Element hai konsa.
+
+// Matlab ke hamne HTML se button ko get kar ke uspe addEvent chlaya or uspe click kiyya (target) ki property laga ke to ye button khud ka address nikal kar dega ke wo hai konsa Element.                          
+
+
+btn2?.addEventListener("click", (e)=>{ // is addEvent ki sari property`s (e) parameter mein aa gai.
+    console.log(e.target);     // or ham is (e) ke andar se sirf (target) ki property se is HTML Element, uski value, or uski id ke sath print krwa rhy hain browser console mein.
+});                            // Output: <button id="btn2">Button2</button>                   
+
+// is button ke (target) ne apni Id or value print kar ke di or opar waly button ne apni. 
+
+
+// input work
+
+myInput?.addEventListener('change', (e)=>{  // input ke andar kuch likh kar enter karenge to ye us input wali value ko dosri value se compare krwa ke dega. change Event se 2 values ko compare karwa sakty hain.
+    console.log(e.target);  // jese hi input ke andar kuch likh kar enter karenge to ye (change) chalega or uska HTML Element bhi laar kar dey dega print console mein.
+});
+
+myInput?.addEventListener('change', (e)=>{
+    console.log(e.target.value);  // (e) yani addEvent method ke Events (target) yani iska khud ka element jisko target kar ke ye Event chlaya jaa rha hai 
+})                                // (value) yani jab ham kuch bhi input mein likh kar enter karenge to wo value hamen browser console mein print ho kar milegi.
+
+
+function myfunc(){  // HTML button Element par jese click karenge uske andar ka onclick ka attribute chalega jiske andar myfunc() ko chlany ke liye calling line di hui hai jese hi wo calling line chalegi typscript mein moujod 
+    console.log("I am function"); // us function ki body ke andar likha code bhi chal jaega.
+}                                //  I am function
